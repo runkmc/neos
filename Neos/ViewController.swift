7eb86300e6c4e912ff14743edbb6378b3512f7e4
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-13 13:10:43 -0800

    Initial Commit

---
+++ Neos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Neos
+//
+//  Created by Kevin McGladdery on 1/13/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
