d06b9d91776109c7971f51985dc5a342360506e0
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-17 14:34:45 -0800

    Add files for Asteroid model

---
+++ Neos/Asteroid.swift
@@ -0,0 +1,9 @@
+//
+//  Asteroid.swift
+//  Neos
+//
+//  Created by Kevin McGladdery on 1/17/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
---
+++ NeosTests/AsteroidSpec.swift
@@ -0,0 +1,11 @@
+//
+//  AsteroidSpec.swift
+//  Neos
+//
+//  Created by Kevin McGladdery on 1/17/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
