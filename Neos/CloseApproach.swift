e80058d6905ab65b6dd1643271b8e779e8cc6bae
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-17 21:19:18 -0800

    CloseApproach parses and stores its data

---
+++ Neos/CloseApproach.swift
@@ -0,0 +1,36 @@
+//
+//  CloseApproach.swift
+//  Neos
+//
+//  Created by Kevin McGladdery on 1/17/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Runes
+import Argo
+import Curry
+
+struct CloseApproach {
+    let closeApproachDate: String
+    let kph: String
+    let mph: String
+    let missDistanceKilometers: String
+    let missDistanceMiles: String
+    let missDistanceLunar: String
+    let missDistanceAstronomical: String
+}
+
+extension CloseApproach: Decodable {
+    static func decode(json: JSON) -> Decoded<CloseApproach> {
+        let c = curry(CloseApproach.init)
+        return c
+            <^> json <| "close_approach_date"
+            <*> json <| ["relative_velocity","kilometers_per_hour"]
+            <*> json <| ["relative_velocity","miles_per_hour"]
+            <*> json <| ["miss_distance","kilometers"]
+            <*> json <| ["miss_distance","miles"]
+            <*> json <| ["miss_distance","lunar"]
+            <*> json <| ["miss_distance","astronomical"]
+    }
+}
---
+++ NeosTests/CloseApproachSpec.swift
@@ -0,0 +1,37 @@
+//
+//  CloseApproachSpec.swift
+//  Neos
+//
+//  Created by Kevin McGladdery on 1/17/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import Curry
+import Runes
+import Argo
+@testable import Neos
+
+class CloseApproachSpec: QuickSpec {
+    override func spec() {
+        describe("A close approach") {
+            let bundle = NSBundle(forClass: self.dynamicType)
+            let path = bundle.pathForResource("testfeed", ofType: "json")
+            let data = NSData(contentsOfFile: path!)
+            let asteroids = jsonParser(data!)
+            let approach = asteroids[0]?.approach[0]
+            
+            it("stores its properties") {
+                expect(approach?.closeApproachDate) == "2016-01-17"
+                expect(approach?.kph) == "53497.777455211"
+                expect(approach?.mph) == "33241.4384415448"
+                expect(approach?.missDistanceKilometers) == "52985684"
+                expect(approach?.missDistanceMiles) == "32923778"
+                expect(approach?.missDistanceLunar) == "137.7789154053"
+                expect(approach?.missDistanceAstronomical) == "0.354187416"
+            }
+        }
+    }
+}
