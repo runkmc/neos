502663b8c8107f1b9c82c3f36c69cccf28f782bd
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-17 21:07:57 -0800

    Asteroid correstly parses and stores data

--- Neos/Asteroid.swift
+++ Neos/Asteroid.swift
@@ -10,3 +10,30 @@ import Foundation
 import Runes
 import Argo
 import Curry
+
+struct Asteroid {
+    let name: String
+    let url: String
+    let estimatedDiameterMaxMeters: Double
+    let estimatedDiameterMinMeters: Double
+    let estimatedDiameterMaxFeet: Double
+    let estimatedDiameterMinFeet: Double
+    let hazardous: Bool
+    let approach: [CloseApproach]
+}
+
+extension Asteroid: Decodable {
+    static func decode(json: JSON) -> Decoded<Asteroid> {
+        let a = curry(Asteroid.init)
+        return a
+            <^> json <| "name"
+            <*> json <| "nasa_jpl_url"
+            <*> json <| ["estimated_diameter","meters","estimated_diameter_max"]
+            <*> json <| ["estimated_diameter","meters","estimated_diameter_min"]
+            <*> json <| ["estimated_diameter","feet","estimated_diameter_max"]
+            <*> json <| ["estimated_diameter","feet","estimated_diameter_min"]
+            <*> json <| "is_potentially_hazardous_asteroid"
+            <*> json <|| "close_approach_data"
+    }
+}
+
--- NeosTests/AsteroidSpec.swift
+++ NeosTests/AsteroidSpec.swift
@@ -9,3 +9,29 @@
 import Foundation
 import Quick
 import Nimble
+import Curry
+import Runes
+import Argo
+@testable import Neos
+
+class AsteroidSpec: QuickSpec {
+    override func spec() {
+        describe("Creating an asteroid") {
+            let bundle = NSBundle(forClass: self.dynamicType)
+            let path = bundle.pathForResource("testfeed", ofType: "json")
+            let data = NSData(contentsOfFile: path!)
+            let asteroids = jsonParser(data!)
+            let asteroid = asteroids[0]
+            
+            it("stores its properties") {
+                expect(asteroid?.name) == "(2011 EK)"
+                expect(asteroid?.url) == "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3558278"
+                expect(asteroid?.estimatedDiameterMaxMeters) == 196.8067450894
+                expect(asteroid?.estimatedDiameterMinMeters) == 88.0146520901
+                expect(asteroid?.estimatedDiameterMaxFeet) == 645.6914415591
+                expect(asteroid?.estimatedDiameterMinFeet) == 288.7619911632
+                expect(asteroid?.hazardous) == false
+            }
+        }
+    }
+}
