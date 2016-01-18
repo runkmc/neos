4d22bd10c43ae50891eaf0f85a4666c2267d3ed3
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-17 21:07:30 -0800

    Create JSON parser

---
+++ Neos/jsonParser.swift
@@ -0,0 +1,27 @@
+//
+//  jsonParser.swift
+//  Neos
+//
+//  Created by Kevin McGladdery on 1/17/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Argo
+
+func jsonParser(data:NSData) -> [Asteroid?] {
+    let json = try? NSJSONSerialization.JSONObjectWithData(data, options: []) as! [String: AnyObject]
+    guard let days = json!["near_earth_objects"] as? [String: [AnyObject]] else {
+        return []
+    }
+    var asteroidJSON = [AnyObject]()
+    days.forEach { _, value in
+        value.forEach { v in
+            asteroidJSON.append(v)
+        }
+    }
+    return asteroidJSON.flatMap {asteroid -> Asteroid? in
+        let a: Asteroid? = decode(asteroid)
+        return a
+    }
+}
