e585c20de6a39672ee5a64383a63e51fd93d8ab9
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-24 21:07:22 -0800

    Add paths as properties

--- Neos/OrbitalView.swift
+++ Neos/OrbitalView.swift
@@ -9,37 +9,37 @@
 import UIKit
 
 class OrbitalView: UIView {
-
+    
+    let sunPath = UIBezierPath(ovalInRect: CGRectMake(62, 62, 114, 114))
+    let innerOrbitPath = UIBezierPath(ovalInRect: CGRectMake(28, 28, 182, 182))
+    let innerPlanetPath = UIBezierPath(ovalInRect: CGRectMake(47, 175, 18, 18))
+    let outerOrbitPath = UIBezierPath(ovalInRect: CGRectMake(2, 2, 234, 234))
+    let outerPlanetPath = UIBezierPath(ovalInRect: CGRectMake(192, 193, 18, 18))
+    
     override func drawRect(rect: CGRect) {
     let color = UIColor(red: 0.920, green: 0.161, blue: 0.478, alpha: 1.000)
     let color2 = UIColor(red: 0.380, green: 0.906, blue: 0.878, alpha: 1.000)
     
     // Sun
-    let ovalPath = UIBezierPath(ovalInRect: CGRectMake(62, 62, 114, 114))
     color.setFill()
-    ovalPath.fill()
+    sunPath.fill()
     
     // Inner orbit
-    let oval2Path = UIBezierPath(ovalInRect: CGRectMake(28, 28, 182, 182))
     color2.setStroke()
-    oval2Path.lineWidth = 2
-    oval2Path.stroke()
+    innerOrbitPath.lineWidth = 2
+    innerOrbitPath.stroke()
     
     // Inner planet
-    let oval3Path = UIBezierPath(ovalInRect: CGRectMake(47, 175, 18, 18))
     color2.setFill()
-    oval3Path.fill()
+    innerPlanetPath.fill()
     
     // Outer orbit
-    let oval4Path = UIBezierPath(ovalInRect: CGRectMake(2, 2, 234, 234))
     color2.setStroke()
-    oval4Path.lineWidth = 2
-    oval4Path.stroke()
+    outerOrbitPath.lineWidth = 2
+    outerOrbitPath.stroke()
     
     // Outer planet
-    let oval5Path = UIBezierPath(ovalInRect: CGRectMake(192, 193, 18, 18))
     color2.setFill()
-    oval5Path.fill()
+    outerPlanetPath.fill()
     }
-
 }
