213c6bf23827f8b2235084116cf5a73e5acb6f34
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-01-25 17:38:28 -0800

    Add NeosCollectionViewCell

---
+++ Neos/NeosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  NeosCollectionViewCell.swift
+//  Neos
+//
+//  Created by Kevin McGladdery on 1/25/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class NeosCollectionViewCell: UICollectionViewCell {
+    
+}
