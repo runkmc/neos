399f5642d962e22a9e28ef0f152a08dded07cb60
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-05-22 16:01:50 -0700

    Remove old tests

--- NeosTests/AsteroidDownloaderSpec.swift
+++ NeosTests/AsteroidDownloaderSpec.swift
@@ -30,26 +30,3 @@ class AsteroidDownloaderTest: XCTestCase {
 
     }
 }
-//class AsteroidDownloaderSpec: QuickSpec {
-//    override func spec() {
-//        describe("The Downloader") {
-//            let calendar = NSCalendar(calendarIdentifier: NSCalendar.Identifier.gregorian)
-//            var startComps = DateComponents()
-//            startComps.year = 1990
-//            startComps.month = 6
-//            startComps.day = 18
-//            let startDate = calendar!.date(from:startComps)!
-//            
-//            var endComps = DateComponents()
-//            endComps.year = 1990
-//            endComps.month = 9
-//            endComps.day = 24
-//            let endDate = calendar!.date(from:endComps)!
-//            let downloader = AsteroidDownloader(startDate: startDate, endDate: endDate)
-//            
-//            it("generates the correct request") {
-//                expect(downloader.requestString) == "https://api.nasa.gov/neo/rest/v1/feed?start_date=1990-06-18&end_date=1990-09-24&api_key=\(ApiKey)"
-//            }
-//        }
-//    }
-//}
