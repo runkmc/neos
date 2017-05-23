6735633e61c9b2c668f6476d856cfd9bf0cbcb06
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-05-23 10:45:01 -0700

    Switch tests to XCTest

--- NeosTests/CloseApproachSpec.swift
+++ NeosTests/CloseApproachSpec.swift
@@ -7,7 +7,26 @@
 //
 
 import Foundation
+import XCTest
 @testable import Neos
+
+class CloseApproachTest: XCTestCase {
+	func testCloseApproachCreation() {
+        let bundle = Bundle(for: type(of: self))
+        let path = bundle.path(forResource:"testfeed", ofType: "json")
+        let data = NSData(contentsOfFile: path!)
+        let asteroids = jsonParser(data! as Data)
+        let approach = asteroids[0]?.approach[0]
+            
+        XCTAssertEqual(approach?.closeApproachDate, "2016-01-17")
+		XCTAssertEqual(approach?.kph, "53497.777455211")
+		XCTAssertEqual(approach?.mph, "33241.4384415448")
+		XCTAssertEqual(approach?.missDistanceKilometers, "52985684")
+		XCTAssertEqual(approach?.missDistanceMiles, "32923778")
+		XCTAssertEqual(approach?.missDistanceLunar, "137.7789154053")
+		XCTAssertEqual(approach?.missDistanceAstronomical, "0.354187416")
+	}
+}
 
 //class CloseApproachSpec: QuickSpec {
 //    override func spec() {
