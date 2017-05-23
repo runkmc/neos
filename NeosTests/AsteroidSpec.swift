793f210f281f4401180479a5ecb3c41625db795a
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-05-23 10:50:27 -0700

    Convert tests to XCTest

--- NeosTests/AsteroidSpec.swift
+++ NeosTests/AsteroidSpec.swift
@@ -7,26 +7,23 @@
 //
 
 import Foundation
+import XCTest
 @testable import Neos
 
-//class AsteroidSpec: QuickSpec {
-//    override func spec() {
-//        describe("Creating an asteroid") {
-//            let bundle = Bundle(for: type(of: self))
-//            let path = bundle.path(forResource: "testfeed", ofType: "json")
-//            let data = NSData(contentsOfFile: path!)
-//            let asteroids = jsonParser(data! as Data)
-//            let asteroid = asteroids[0]
-//            
-//            it("stores its properties") {
-//                expect(asteroid?.name) == "(2011 EK)"
-//                expect(asteroid?.url) == "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3558278"
-//                expect(asteroid?.estimatedDiameterMaxMeters) == 196.8067450894
-//                expect(asteroid?.estimatedDiameterMinMeters) == 88.0146520901
-//                expect(asteroid?.estimatedDiameterMaxFeet) == 645.6914415591
-//                expect(asteroid?.estimatedDiameterMinFeet) == 288.7619911632
-//                expect(asteroid?.hazardous) == false
-//            }
-//        }
-//    }
-//}
+class AsteroidTest: XCTestCase {
+	func testAsteroidCreation() {
+       let bundle = Bundle(for: type(of: self))
+       let path = bundle.path(forResource: "testfeed", ofType: "json")
+       let data = NSData(contentsOfFile: path!)
+       let asteroids = jsonParser(data! as Data)
+       let asteroid = asteroids[0]
+            
+       XCTAssertEqual(asteroid?.name, "(2011 EK)")
+       XCTAssertEqual(asteroid?.url, "http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=3558278")
+       XCTAssertEqual(asteroid?.estimatedDiameterMaxMeters, 196.8067450894)
+       XCTAssertEqual(asteroid?.estimatedDiameterMinMeters, 88.0146520901)
+       XCTAssertEqual(asteroid?.estimatedDiameterMaxFeet, 645.6914415591)
+       XCTAssertEqual(asteroid?.estimatedDiameterMinFeet, 288.7619911632)
+       XCTAssertEqual(asteroid?.hazardous, false)
+	}
+}
