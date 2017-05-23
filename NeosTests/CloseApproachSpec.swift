c61a0f5f40a40ce357da0d425b3c28ee105f3b03
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-05-23 10:45:59 -0700

    Remove old tests

--- NeosTests/CloseApproachSpec.swift
+++ NeosTests/CloseApproachSpec.swift
@@ -27,25 +27,3 @@ class CloseApproachTest: XCTestCase {
 		XCTAssertEqual(approach?.missDistanceAstronomical, "0.354187416")
 	}
 }
-
-//class CloseApproachSpec: QuickSpec {
-//    override func spec() {
-//        describe("A close approach") {
-//            let bundle = Bundle(for: type(of: self))
-//            let path = bundle.path(forResource:"testfeed", ofType: "json")
-//            let data = NSData(contentsOfFile: path!)
-//            let asteroids = jsonParser(data! as Data)
-//            let approach = asteroids[0]?.approach[0]
-//            
-//            it("stores its properties") {
-//                expect(approach?.closeApproachDate) == "2016-01-17"
-//                expect(approach?.kph) == "53497.777455211"
-//                expect(approach?.mph) == "33241.4384415448"
-//                expect(approach?.missDistanceKilometers) == "52985684"
-//                expect(approach?.missDistanceMiles) == "32923778"
-//                expect(approach?.missDistanceLunar) == "137.7789154053"
-//                expect(approach?.missDistanceAstronomical) == "0.354187416"
-//            }
-//        }
-//    }
-//}
